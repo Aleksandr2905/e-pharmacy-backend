f473a78d58fe817414224cf6c626a648f28fdebc
Aleksandr2905 <natalukha@ukr.net>
2024-05-08 22:19:55 +0300

    Update reviews-controller.js

--- controllers/reviews-controller.js
+++ controllers/reviews-controller.js
@@ -2,7 +2,8 @@ import { ctrlWrapper } from "../decorators/index.js";
 import Reviews from "../models/Reviews.js";
 
 const getReviews = async (req, res) => {
-  const result = await Reviews.find();
+  const { limit = 3 } = req.body;
+  const result = await Reviews.find().limit(limit);
 
   res.json(result);
 };
