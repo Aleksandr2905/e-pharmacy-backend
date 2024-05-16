564e422d87e45abb3b7d02e26bf5ef77db73e42d
Aleksandr2905 <natalukha@ukr.net>
2024-05-16 19:38:10 +0300

    fix

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -52,17 +52,10 @@ const signin = async (req, res) => {
   });
 };
 
-const getUserInfo = async (req, res) => {
+const getUserInfo = (req, res) => {
   const { username, email } = req.user;
 
-  const user = await User.findOne({ email });
-
-  res.json({
-    user: {
-      username: user.username,
-      email: user.email,
-    },
-  });
+  res.json({ username, email });
 };
 
 const logout = async (req, res) => {
