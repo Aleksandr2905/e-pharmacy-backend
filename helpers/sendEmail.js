d29cfb2bb1cc247de6ebd2d74d27c4535bc11a54
Aleksandr2905 <natalukha@ukr.net>
2024-05-23 20:16:55 +0300

    fix

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -5,8 +5,8 @@ const { UKR_NET_PASSWORD, UKR_NET_FROM } = process.env;
 
 const nodemailerConfig = {
   host: "smtp.ukr.net",
-  port: 2525,
-  secure: true,
+  port: 465,
+  // secure: true,
   auth: {
     user: UKR_NET_FROM,
     pass: UKR_NET_PASSWORD,
